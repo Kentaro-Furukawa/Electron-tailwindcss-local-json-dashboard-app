8bd60a7611a48b1f83caa7e9f289f59911ba68bd
Ken <info@ken89.com>
2022-04-19 10:53:37 +0900

    Add sidebar element and style.

--- renderer.js
+++ renderer.js
@@ -1,1 +1,41 @@
+const stateIconButtons = document.querySelector('.state-icon-buttons');
+
+const stateIcons = [
+    { icon: 'user-check', caption: 'Available' },
+    { icon: 'phone-call', caption: 'Next up' },
+    { icon: 'headphones', caption: 'On call' },
+    { icon: 'pen-tool', caption: 'Issuing' },
+    { icon: 'radio', caption: 'Announce' },
+    { icon: 'user-x', caption: 'Away' }
+];
+
+stateIcons.forEach(iconItem => {
+    const iconElement = document.createElement('div');
+    iconElement.classList.add("sidebar-icon", "group")
+    iconElement.innerHTML =
+        `
+    <i data-feather="${iconItem.icon}"></i>
+    <span class="sidebar-caption group-hover:scale-100">
+    ${iconItem.caption}
+    </span>
+    `
+    stateIconButtons.appendChild(iconElement);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ****************************************
+
 feather.replace()
+
