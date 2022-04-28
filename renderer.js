2a574e9b165198084d6592b2f36edb56a5f3e3b4
Ken <info@ken89.com>
2022-04-29 05:44:17 +0900

    update incPattern to includes task pattern.

--- renderer.js
+++ renderer.js
@@ -1,5 +1,5 @@
 const adminPassword = 'admin';
-const incPattern = /(INC)\d{7}(?!\d)/gi;
+const incPattern = /(INC)\d{7}(?!\d)|(TASK)\d{7}(?!\d)/gi;
 let requestedRecord = '';
 let copiedItem = '';
 const currentStateIcon = document.querySelector('.current-state-icon');
