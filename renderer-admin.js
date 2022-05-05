d9862cf8daffe5d60258b7b4cdf8c4995645f9e5
Ken <info@ken89.com>
2022-05-05 10:51:41 +0900

    click submit btn, then if any diff between org list and input list, show msg in the cosole.

--- renderer-admin.js
+++ renderer-admin.js
@@ -1,5 +1,8 @@
 document.querySelector('#admin-username').innerText = `${localStorage.user}`;
+let orgUserList = [];
+let inputDataArray = [];
 const userListInputArea = document.querySelector('#userlist-input');
+const userlistSubmit = document.querySelector('#userlist-submit');
 
 window.addEventListener('load', (event) => {
     getUserList();
@@ -7,10 +10,27 @@ window.addEventListener('load', (event) => {
 
 
 const getUserList = async () => {
-    let userList = await window.api.requestUserList();
-    console.log(userList);
-    userListInputArea.textContent = userList.join('\r\n');
+    orgUserList = await window.api.requestUserList();
+    userListInputArea.value = orgUserList.join('\n');
 }
+
+const arrayEquals = (a, b) => {
+    return Array.isArray(a) &&
+      Array.isArray(b) &&
+      a.length === b.length &&
+      a.every((val, index) => val.trim() === b[index].trim());
+  }
+  
+userlistSubmit.addEventListener('click' ,(e) => {
+    e.preventDefault();
+    let inputData = userListInputArea.value
+    inputDataArray = inputData.split('\n');
+    if (!(arrayEquals(orgUserList, inputDataArray))){
+        console.log('some change')
+    }
+  
+});
+
 
 
 
